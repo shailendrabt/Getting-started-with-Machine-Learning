1782d2e31a8f0be26da6b5b6273cd723e944b6ba
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-09-05 16:44:09 +0530

    Add files via upload

---
+++ Python/booleans.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 15:00:28 2024
+
+@author: bioinfo
+"""
+
+# Python Booleans
+# Booleans represent one of two values: True or False.
+
+# Boolean Values
+# In programming you often need to know if an expression is True or False.
+
+# You can evaluate any expression in Python, and get one of two answers, True or False.
+
+# When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# Example
+# Print a message based on whether the condition is True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
